e89fdc6d6fbbd211c40a696dd1d07e079830ab08
macbook <wnfid1040@gmail.com>
2022-12-26 02:54:41 +0900

    2022.12.26  ~ing

---
+++ JuRyang/programmers/EX_소수찾기.java
@@ -0,0 +1,45 @@
+package JuRyang.programmers;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+import static JuRyang.programmers.EX_치킨쿠폰.solution;
+
+public class EX_소수찾기 {
+    /**
+     1부터 입력받은 숫자 n 사이에 있는 소수의 개수를 반환하는 함수, solution을 만들어 보세요.
+     소수는 1과 자기 자신으로만 나누어지는 수를 의미합니다.
+            (1은 소수가 아닙니다.)
+
+    제한 조건
+    n은 2이상 1000000이하의 자연수입니다.
+
+     입출력 예
+            n	result
+            10	4               1 2 3 4 5 6 7 8 9 10
+             5	3               5 4 3 2 1
+    입출력 예 #1
+            1부터 10 사이의 소수는 [2,3,5,7] 4개가 존재하므로 4를 반환
+
+    입출력 예 #2
+            1부터 5 사이의 소수는 [2,3,5] 3개가 존재하므로 3를 반환
+     */
+
+        public static int solution(int n) {
+            int answer = 0;
+            int [] arr = new int [n];
+            for(int i = 0; i < 10; i++){
+                for(int j = 2; j <= n; j++){
+
+                }
+            }
+
+            return answer;
+        }
+
+
+    public static void main(String[] args) {
+        solution(10);
+
+    }
+}
---
+++ JuRyang/programmers/EX_잘라서배열로저장.java
@@ -0,0 +1,47 @@
+package JuRyang.programmers;
+
+import java.util.*;
+
+public class EX_잘라서배열로저장 {
+
+    /**
+     * 문자열 my_str과 n이 매개변수로 주어질 때, my_str을 길이 n씩 잘라서 저장한 배열을 return하도록 solution 함수를 완성해주세요.
+     *
+     *  my_str	                n	result
+
+     * "abc1Addfggg4556b"	    6	["abc1Ad", "dfggg4", "556b"]
+     * "abcdef123"	            3	["abc", "def", "123"]
+     * */
+
+    public static String[] solution(String my_str, int n) {
+        String[] answer = {};
+        String[] result = my_str.split("");
+        System.out.println(Arrays.toString(result)); //[a, b, c, 1, A, d, d, f, g, g, g, 4, 5, 5, 6, b]
+
+        String aa = my_str.substring(0,6);
+        System.out.println(aa+"<"); //abc1A<
+
+       // ArrayList<String> io = new ArrayList<>();
+        Map io = new HashMap();
+        for(int i = 0; i<result.length / n; i++){
+           for(int  j = i*n ; j < i*n+n ; j ++){
+                String a = my_str.substring(i,n);
+                io.put(i,a);
+            }
+        }
+
+        System.out.println(io.values()+"answer");
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        EX_잘라서배열로저장 aa = new EX_잘라서배열로저장();
+        EX_잘라서배열로저장 aa2 = new EX_잘라서배열로저장();
+        aa.solution("abc1Addfggg4556b",6);
+       // aa2.solution("abcdef123",3);
+
+
+    }
+}
